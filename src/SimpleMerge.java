3b4e15b0063dbc5c53a8beca53d4b94d5e70f02c
baishakicodes <43207472+baishakicodes@users.noreply.github.com>
2019-01-31 20:29:29 -0500

    fixed simple merge

--- src/SimpleMerge.java
+++ src/SimpleMerge.java
@@ -6,27 +6,43 @@
  */
 public class SimpleMerge {
     public static int[] simpleMerge(int[] arr1, int[] arr2) {
-        int[] newArr = new int[arr1.length+arr2.length];
-        int c=0;
-        for(int i=0; i<arr1.length;i++){
-            if(arr1[i]<arr2[i]){
-                newArr[i]=arr1[i];
-            }
-            c=i;
-            if(arr1[i]>arr2[i]){
-                newArr[i]=arr2[i];
-            }
-            c=i;
-            if(arr1[i]<arr2[c]){
-                newArr[i]=arr1[i];
-            }
-            else{
-                newArr[i]=arr2[c];
+        //create a new a array that will hold the merged sorted array
+        int[] newArr = new int[arr1.length + arr2.length];
+
+        //keeping track of the indices of each of the arrays
+        int arrOne = 0;
+        int arrTwo = 0;
+
+        //keeping rack of the newArr index
+        int newArrInd = 0;
+
+        //This is the loop that checks the values of the indexes of both arrays
+        while (arrOne < arr1.length && arrTwo < arr2.length) {
+            if (arr1[arrOne] < arr2[arrTwo]) {
+                newArr[newArrInd] = arr1[arrOne];
+                newArrInd++;
+                arrOne++;
+            } else {
+                newArr[newArrInd] = arr2[arrTwo];
+                newArrInd++;
+                arrTwo++;
             }
 
         }
+        //once the upper while loop is done we need to copy the rest of the remaining
+        //values of any array that hasn't reached it's end first
+        while(arrOne<arr1.length){
+            newArr[newArrInd]=arr1[arrOne];
+            newArrInd++;
+            arrOne++;
+        }
+        while(arrTwo<arr2.length){
+            newArr[newArrInd]=arr2[arrTwo];
+            newArrInd++;
+            arrTwo++;
+        }
         return newArr;
-
     }
 
 }
+
